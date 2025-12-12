cdbc075f5783f56db4375fc373b4e5de698c1395
John Arban Lewis <johnarban@users.noreply.github.com>
2025-12-11 20:39:52 -0500

    add name space

--- src/components/plotly/PlotlyGraph.vue
+++ src/components/plotly/PlotlyGraph.vue
@@ -99,6 +99,7 @@ const plotlyData = computed<Plotly.PlotData[]>(() => {
   datasets.forEach((data, index) => {
     // create a hash from the data.x and data.y to be it's "id"
     data = filterNulls.value ? filterNullValues(data) : data;
+    data.name = data.name +  + '　'; // add an ideographic space for for legend uniqueness
     
     // check x, and have same length y
     if (!data.x || !data.y || data.x.length !== data.y.length) {
