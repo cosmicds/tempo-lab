7a3fb1c2429752f95fa327d16562851956e99e2a
John Arban Lewis <johnarban@users.noreply.github.com>
2026-01-12 20:55:44 -0500

    create legends for aqi and landuse

---
+++ src/components/AQILegend.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import LegendBase from "@/components/LegendBase.vue";
+
+const data = [
+  {
+    name: "good",
+    label: "Good (0–50)",
+    color: "#00e400",
+    text: "Air quality is satisfactory, and air pollution poses little or no risk.",
+  },
+  {
+    name: "moderate",
+    label: "Moderate (51–100)",
+    color: "#ffff00",
+    text: "Air quality is acceptable. However, there may be a risk for some people, particularly those who are unusually sensitive to air pollution.",
+  },
+  {
+    name: "usg",
+    label: "Unhealthy for Sensitive Groups (101–150)",
+    color: "#ff7e00",
+    text: "Members of sensitive groups may experience health effects. The general public is less likely to be affected.",
+  },
+  {
+    name: "unhealthy",
+    label: "Unhealthy (151–200)",
+    color: "#ff0000",
+    text: "Some members of the general public may experience health effects; members of sensitive groups may experience more serious health effects.",
+  },
+  {
+    name: "very-unhealthy",
+    label: "Very Unhealthy (201–300)",
+    color: "#8f3f97",
+    text: "Health alert: The risk of health effects is increased for everyone.",
+  },
+  {
+    name: "hazardous",
+    label: "Hazardous (301+)",
+    color: "#7e0023",
+    text: "Health warning of emergency conditions: everyone is more likely to be affected.",
+  },
+];
+</script>
+
+<template>
+  <LegendBase
+    class="aqi-legend"
+    :data="data"
+    info-text="Learn more about Air quality colors."
+    citation="Source: U.S. EPA - AirNow"
+  />
+</template>
+
+<style>
+
+
+.long-legend > .legend-label {
+  grid-template-columns: minmax(auto, 200px) 1fr;
+}
+
+</style>
---
+++ src/components/LandUseLegend.vue
@@ -0,0 +1,78 @@
+<script setup lang="ts">
+import LegendBase from "@/components/LegendBase.vue";
+const data = [
+  { name: "built",
+    label: "Built Area",
+    color: "#ed242b",
+    text: "Human made structures; major road and rail networks; large homogenous impervious surfaces including parking structures, office buildings and residential housing; examples: houses, dense villages / towns / cities, paved roads, asphalt."
+  },
+  
+  { name: "water",
+    label: "Water",
+    color: "#1b5bab",
+    text: "Areas where water was predominantly present throughout the year; may not cover areas with sporadic or ephemeral water; contains little to no sparse vegetation, no rock outcrop nor built up features like docks; examples: rivers, ponds, lakes, oceans, flooded salt plains.",
+  },
+  
+  { name: "trees",
+    label: "Trees",
+    color: "#358221",
+    text: "Any significant clustering of tall (~15 feet or higher) dense vegetation, typically with a closed or dense canopy; examples: wooded vegetation, clusters of dense tall vegetation within savannas, plantations, swamp or mangroves (dense/tall vegetation with ephemeral water or canopy too thick to detect water underneath).",
+  },
+  
+  { name: "crops",
+    label: "Crops",
+    color: "#fcdb5c",
+    text: "Human planted/plotted cereals, grasses, and crops not at tree height; examples: corn, wheat, soy, fallow plots of structured land.",
+  },
+  
+  { name: "rangeland",
+    label: "Rangeland",
+    color: "#efcfa8",
+    text: "Open areas covered in homogenous grasses with little to no taller vegetation; wild cereals and grasses with no obvious human plotting (i.e., not a plotted field); examples: natural meadows and fields with sparse to no tree cover, open savanna with few to no trees, parks/golf courses/lawns, pastures. Mix of small clusters of plants or single plants dispersed on a landscape that shows exposed soil or rock; scrub-filled clearings within dense forests that are clearly not taller than trees; examples: moderate to sparse cover of bushes, shrubs and tufts of grass, savannas with very sparse grasses, trees or other plants.",
+  },
+  
+  { name: "flooded",
+    label: "Flooded Vegetation",
+    color: "#87d19e",
+    text: "Areas of any type of vegetation with obvious intermixing of water throughout a majority of the year; seasonally flooded area that is a mix of grass/shrub/trees/bare ground; examples: flooded mangroves, emergent vegetation, rice paddies and other heavily irrigated and inundated agriculture.",
+  },
+
+  { name: "bare",
+    label: "Bare Ground",
+    color: "#ede9e4",
+    text: "Areas of rock or soil with very sparse to no vegetation for the entire year; large areas of sand and deserts with no to little vegetation; examples: exposed rock or soil, desert and sand dunes, dry salt flats/pans, dried lake beds, mines.",
+  },
+  
+  { name: "snow",
+    label: "Snow/Ice",
+    color: "#f2faff",
+    text: "Large homogenous areas of permanent snow or ice, typically only in mountain areas or highest latitudes; examples: glaciers, permanent snowpack, snow fields.",
+  },
+  
+  { name: "clouds",
+    label: "Clouds",
+    color: "#c8c8c8",
+    text: "No land cover information due to persistent cloud cover.",
+  }
+];
+
+</script>
+
+<template>
+  <LegendBase
+    class="land-use-legend"
+    :data="data"
+    info-text="Learn more about Land use colors."
+    citation="Source: ESRI Living Atlas: Sentinel-2 10m Land Use/Land Cover Time Series by Impact Observatory"
+  />
+
+</template>
+
+<style scoped>
+
+
+.long-legend > .legend-label {
+  grid-template-columns: minmax(auto, 100px) 1fr;
+}
+
+</style>
