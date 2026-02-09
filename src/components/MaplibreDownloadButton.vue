578a4ffe9ab67fba892f5f0e9a1c8dcd6e4befc3
John Arban Lewis <johnarban@users.noreply.github.com>
2026-02-09 18:38:59 -0500

    fix console warning about mismatched type (map could be null)

--- src/components/MaplibreDownloadButton.vue
+++ src/components/MaplibreDownloadButton.vue
@@ -12,13 +12,14 @@ import { downloadMap } from '@/utils/canvas_downloader';
 import { type Map } from 'maplibre-gl';
 
 interface MaplibreDownloadButtonProps {
-  map: Map,
+  map: Map | null,
   filename: string
 }
 
 const props = defineProps<MaplibreDownloadButtonProps>();
 
 const onClick = () => {
+  if (!props.map) return;
   downloadMap(props.map, props.filename);
 };
 </script>
