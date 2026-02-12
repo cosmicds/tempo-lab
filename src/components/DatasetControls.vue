afb4e29c8f1da3144773026b566ca169b4f1b67b
John Arban Lewis <johnarban@users.noreply.github.com>
2026-02-12 13:45:39 -0500

    use aggregation specific naming

--- src/components/DatasetControls.vue
+++ src/components/DatasetControls.vue
@@ -786,6 +786,16 @@ function openAggregationDialog(selection: UserDataset) {
   aggregationDataset.value = selection;
   showAggregationDialog.value = true;
 }
+
+function easyNames(short: FoldType) {
+  if (short.toLowerCase().endsWith('none')) {
+    return 'Binned ' + short.toLowerCase().split('of')[0] + 'ly';
+  }
+  if (short.toLowerCase().startsWith('none')) {
+    return 'Stacked by ' + short.toLowerCase().split('of')[1];
+  }
+  return camelToSentance(short);
+}
 function handleAggregationSaved(aggregatedSelection: UserDataset) {
   const n = datasets.value
     .filter(d => !!d.folded) // only count folded datasets
@@ -793,12 +803,16 @@ function handleAggregationSaved(aggregatedSelection: UserDataset) {
       // make sure they all have the same parent
       if (d.folded.parent && aggregatedSelection.folded.parent) {
         // check the id (this is stable and always unique)
-        return (aggregatedSelection.folded.parent as UserDataset).id === (d.folded.parent as UserDataset).id;
+        if ((aggregatedSelection.folded.parent as UserDataset).id === (d.folded.parent as UserDataset).id) {
+          if (aggregatedSelection.folded.foldType === d.folded.foldType) {
+            return true;
+          }
+        }
       }
       return false;
     }).length;
   // aggregatedSelection.name = `${aggregatedSelection.name} ${String.fromCharCode(97 + n)}`; // a, b, c, ...
-  aggregatedSelection.name = (aggregatedSelection.name ?? '(Aggregation)').replace('Aggregation', `Aggregation ${String.fromCharCode(97 + n)}`); // a, b, c, ...
+  aggregatedSelection.name = (aggregatedSelection.name ?? '(Aggregation)').replace('Aggregation', `${easyNames(aggregatedSelection.folded.foldType)} ${String.fromCharCode(97 + n)}`);
   store.addDataset(aggregatedSelection, false); // no need to fetch anything
   showAggregationDialog.value = false;
   aggregationDataset.value = null;
@@ -816,6 +830,8 @@ function handleDatasetCreated(dataset: UserDataset) {
 
 import UserDatasetEditor from "./UserDatasetEditor.vue";
 import { contrastingColor } from "@/utils/color";
+import { FoldType } from "@/esri/services/aggregation";
+import { camelToSentance } from "@/utils/text";
 const showDatasetEditor = ref(false);
 const datasetEditorNameOnly = ref(false);
 function handleEditDataset(dataset: UserDataset, nameOnly = false) {
